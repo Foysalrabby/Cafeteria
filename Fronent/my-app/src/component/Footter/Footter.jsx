c042a349817cb59ccac202815e0ea5a61a479434
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-29 19:34:36 +0600

    footer done

--- Fronent/my-app/src/component/Footter/Footter.jsx
+++ Fronent/my-app/src/component/Footter/Footter.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import './Footer.css';
 
 const Footter = () => {
     return (
-        <div>
-            <h2> Footter</h2>
+        <div className='Fottercointainer'>
+       
+        <div className='footeritem'>
+       
+        <p>This is footter</p>
+         <p>This is footter</p>
+         <p>This is footter</p>
+       
         </div>
+
+        <div className='footeritem'>
+     
+        <p>This is footter</p>
+         <p>This is footter</p>
+         <p>This is footter</p>
+        
+        </div>
+
+        <div className='footeritem'>
+        
+        <p>This is footter</p>
+         <p>This is footter</p>
+         <p>This is footter</p>
+        </div>
+      
+        
+        </div>
+        
+       
     );
 };
 
--- Fronent/my-app/src/pages/Homepage/Home.jsx
+++ Fronent/my-app/src/pages/Homepage/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './Home.css';
 import Navbar from '../../component/Navbar/Navbar';
 import Header from '../../component/Header/Header';
+import Footter from '../../component/Footter/Footter';
 const Homepage = () => {
     return (
         <div>
             <Navbar/>
             <Header/>
+            <Footter/>
         </div>
     );
 };
