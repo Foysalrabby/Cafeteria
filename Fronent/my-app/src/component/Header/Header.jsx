0eb54039b6bf66a7346ae587ab25b06b71230279
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-29 18:03:17 +0600

    slide down

--- Fronent/my-app/src/component/Header/Header.jsx
+++ Fronent/my-app/src/component/Header/Header.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import hutufafe from "../../images/hstucafe.jpg";
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1> Header </h1>
+        <div className='Header'>
+        <div className='HeaderContainer'>
+        <Carousel>
+      <Carousel.Item >
+      <img  src={hutufafe} alt="Example" width="1000px" height="500px" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img  src={hutufafe} alt="Example" width="1000px" height="500px" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img  src={hutufafe} alt="Example" width="1000px" height="500px" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+        </div>
+           
         </div>
     );
 };
--- Fronent/my-app/src/pages/Homepage/Home.jsx
+++ Fronent/my-app/src/pages/Homepage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../../component/Navbar/Navbar';
+import Header from '../../component/Header/Header';
 const Homepage = () => {
     return (
         <div>
             <Navbar/>
+            <Header/>
         </div>
     );
 };
