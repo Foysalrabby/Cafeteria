65607bbce9dcdabf851ad9317cb1e4d56106e63b
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-29 16:20:28 +0600

    start header work

---
+++ Fronent/my-app/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+        <div className='Navbarcontainer'>
+        <span> Cafetria</span>
+        <div className='navitem'>
+        <button className='navButton'> Singup</button>
+        <button className='navButton'> LogIn</button>
+        </div>
+
+
+        </div>
+
+
+
+            
+        </div>
+    );
+};
+
+export default Navbar;
